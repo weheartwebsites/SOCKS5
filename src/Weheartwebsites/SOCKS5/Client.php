341965bcdb113a6da2572bd736a96977017798f1
Gunter Grodotzki <gunter@grodotzki.co.za>
2016-07-27 16:14:57 +0200

    fix typos

--- src/Weheartwebsites/SOCKS5/Client.php
+++ src/Weheartwebsites/SOCKS5/Client.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the SOCKS5 library.
  *
- * (c) Gunter Grodotzki <guenter@weheartwebsites>
+ * (c) Gunter Grodotzki <guenter@weheartwebsites.de>
  *
  * For the full copyright and license information, please view the LICENSE file
  * that was distributed with this source code.
